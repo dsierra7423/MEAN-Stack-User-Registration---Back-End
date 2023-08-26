c5d037d614c63d8c390a847894b87083380f6c29
doswaldo7423 <daniel.sierra@easytrack.mx>
2023-08-25 18:00:45 -0600

    Hello world

--- Project/app.js
+++ Project/app.js
@@ -1,31 +1,11 @@
-require('./config/config');
-require('./models/db');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const rtsIndex = require('./routes/index.router');
-
-var app = express();
-
-// middleware
-app.use(bodyParser.json());
-
-app.use(cors({
-    origin: '*'
-}));
-
-app.use('/api', rtsIndex);
-
-// error handler
-app.use((err, req, res, next) => {
-    if (err.name === 'ValidationError') {
-        var valErrors = [];
-        Object.keys(err.errors).forEach(key => valErrors.push(err.errors[key].message));
-        res.status(422).send(valErrors)
-    }
-});
-
-// start server
-app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
